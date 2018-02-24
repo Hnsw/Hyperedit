eecc5e2fce67ef51d8379d2d52fcf95608cdedfe
Hnsw <36800288+Hnsw@users.noreply.github.com>
2018-02-24 22:48:52 +0100

    asdf

--- src/HypereditPackage/Main.java
+++ src/HypereditPackage/Main.java
@@ -1,24 +1,75 @@
 package HypereditPackage;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
+import javax.swing.text.BadLocationException;
 
 public class Main {
 
 	public static void main(String[] args) {
+
 		JFrame frame = new JFrame("Editor");
+		FlowLayout LayoutManager = new FlowLayout();
+		LayoutManager.setAlignment(FlowLayout.CENTER);
+		frame.setLayout(LayoutManager);
+		Color bgColor = new Color(0, 0, 100);
+		frame.getContentPane().setBackground(bgColor);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(800, 900);
-		JTextArea textArea = new JTextArea();
-		textArea.setWrapStyleWord(true);
-		textArea.setSize(800, 900);
-		textArea.setLineWrap(true);
-		Font font = textArea.getFont();
-	//	float size = font.getSize() + 1.0f;
-		float size = 20;
-		textArea.setFont( font.deriveFont(size) );
-		frame.add(textArea);
 		frame.setVisible(true);
+
+		JButton increaseSize = new JButton("bigger");
+		frame.add(increaseSize);
+		increaseSize.setVisible(true);
+
+		JTextPane textArea = new JTextPane();
+		frame.add(textArea);
+		textArea.setPreferredSize(new Dimension(750, 850));
+		textArea.setVisible(true);
+		textArea.setBackground(new Color(10, 255, 255));
+
+		increaseSize.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				float size = textArea.getFont().getSize() + 1.0f;
+				textArea.setFont(textArea.getFont().deriveFont(size));
+			}
+		});
+
+		textArea.addCaretListener(new CaretListener() {
+
+			@Override
+			public void caretUpdate(CaretEvent e) {
+				int dot = e.getDot();
+				int mark = e.getMark();
+				if (dot != mark) {
+					if (dot < mark) {
+						int temp = dot;
+						dot = mark;
+						mark = temp;
+					}
+					int length = dot - mark;
+					String selectedText = "noSelection";
+					try {
+						selectedText = textArea.getText(mark, length);
+					} catch (BadLocationException e1) {
+						e1.printStackTrace();
+					}
+					if (selectedText.contains(">") == true) {
+
+					} else {
+						textArea.replaceSelection(">" + selectedText);
+					}
+
+				}
+			}
+		});
 	}
 
 }
